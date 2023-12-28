30cadc7ecacff531b00115ec99e1990c4c5f5723
Dennis <suwatchai.kaew@gmail.com>
2023-12-28 11:02:11 +0700

    feat: add finished HH

--- src/slices/messageBoardSlice.jsx
+++ src/slices/messageBoardSlice.jsx
@@ -1,1 +1,22 @@
 // TODO: Start Create Slice Here
+import { createSlice } from "@reduxjs/toolkit";
+const allState = {
+  initialMessage: ["Hello all ! This is first message."],
+};
+export const messageSlice = createSlice({
+  name: "message",
+  initialState: allState.initialMessage,
+  reducers: {
+    addMessage: (state, action) => {
+      state = [...state, action.payload];
+      return state;
+    },
+    removeMessage: (state, action) => {
+      state.splice(action.payload, 1);
+      return state;
+    },
+  },
+});
+
+export const { addMessage, removeMessage } = messageSlice.actions;
+export default messageSlice.reducer;
